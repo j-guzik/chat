599bdcaa3c8fdc9d90c2f9ccd2ceae22f89982ef
Justyna Guzik <justyna.b.guzik@gmail.com>
2023-04-01 20:39:19 +0200

    user data store in redux

--- frontend/src/store/actions/authAction.js
+++ frontend/src/store/actions/authAction.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { REGISTER_FAIL, REGISTER_SUCCESS } from "../types/authType";
 
 export const userRegister = (data) => {
   return async (dispatch) => {
@@ -15,9 +16,21 @@ export const userRegister = (data) => {
         data,
         config
       );
-      console.log(response.data);
+      localStorage.setItem("authToken", response.data.token);
+      dispatch({
+        type: REGISTER_SUCCESS,
+        payload: {
+          successMessage: response.data.successMessage,
+          token: response.data.token,
+        },
+      });
     } catch (error) {
-      console.log(error.response.data);
+      dispatch({
+        type: REGISTER_FAIL,
+        payload: {
+          error: error.response.data.error.errorMessage,
+        },
+      });
     }
   };
 };
